ef83ab1c0c73a4ef864c27cbe4a6300de7aea250
hirakamu <achmadirsyadhamizan@gmail.com>
2025-10-16 19:22:40 +0700

    making sure the system has the folder

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from config import ROOT, SESSION_LIFETIME_DAYS, DB_FILE
+from config import ROOT, SESSION_LIFETIME_DAYS, DB_FILE, USERDATA
 import os
 from datetime import timedelta
 #   from routes.admin import bp as admin_bp
@@ -10,6 +10,7 @@ from dbapi import DButils
 from sqlite3 import Connection as sqlite
 
 def create_app():
+    os.makedirs(USERDATA, exist_ok=True)
     db = sqlite(DB_FILE)
     app = Flask(__name__, static_folder=str(ROOT / 'web/static'), template_folder=str(ROOT / 'web/templates'))
     app.config['JSON_SORT_KEYS'] = False
