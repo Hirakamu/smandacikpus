4510316aa15f6930f6821fb98cd052435f0c1fcd
hirakamu <achmadirsyadhamizan@gmail.com>
2025-10-23 13:10:13 +0700

    modify a little bit

--- server/app.py
+++ server/app.py
@@ -5,19 +5,23 @@ from datetime import timedelta
 from routes.admin import bp as admin_bp
 from routes.api import bp as api_bp
 from routes.site import bp as site_bp
-from errors import register_error_handlers
 import datacontroller as datac
+from errors import register_error_handlers
+from dbapi import DButils
 
 def create_app():
     os.makedirs(USERDATA, exist_ok=True)
     app = Flask(__name__, static_folder=str(ROOT / 'web/static'), template_folder=str(ROOT / 'web/templates'))
     app.permanent_session_lifetime = timedelta(days=SESSION_LIFETIME_DAYS)
-    app.secret_key = "your-unique-secret-key"  # Set a unique and secret key for session management
+    app.secret_key = "development-secret"
 
     app.register_blueprint(admin_bp)
     app.register_blueprint(api_bp)
     app.register_blueprint(site_bp)
 
     register_error_handlers(app)
-
+    
+    with app.app_context():
+        DButils.init_db()
+    
     return app
